ededd70ec3ef222972bbf2440afba34ddd59f313
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2026-03-26 23:02:03 -0400

    Update version.php
    updating version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_evalimport';
-$plugin->version = 2026030800;
+$plugin->version = 2026032600;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.1.0';
+$plugin->release = '0.1.1';
